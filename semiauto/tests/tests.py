8dcd5dbdc6de309ff2078d7a6a9bd814ab10f7da
Andreas Tolfsen <ato@mozilla.com>
2014-03-19 16:22:37 +0000

    Increase default timeout

--- semiauto/tests/tests.py
+++ semiauto/tests/tests.py
@@ -3,6 +3,7 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from tornado import testing
+import sys
 import tornado
 
 from marionette import Marionette, MarionetteException
@@ -11,10 +12,13 @@ from semiauto import environment
 from semiauto.environment import InProcessTestEnvironment
 
 
+default_timeout = 600  # 10 minutes
+
+
 # TODO(ato): Find better name: interaction_test? interact?
 def test(*args, **kwargs):
     """Wraps ``tornado.testing.gen_test``."""
-    return tornado.testing.gen_test(*args, **kwargs)
+    return tornado.testing.gen_test(timeout=default_timeout, *args, **kwargs)
 
 
 class TestCase(tornado.testing.AsyncTestCase):
